7acc9dcc19d534eeb7563900316cfee087984502
David Torras <davidtorrasceron@gmail.com>
2022-10-28 11:43:44 +0200

    fix: component namespace

--- resources/views/pad.blade.php
+++ resources/views/pad.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ route('sign-pad::signature')}}" method="POST">
     @csrf
     <div style="text-align: center">
-        <x-signature-pad
+        <x-laravel-sign-pad::signature-pad
             width="500"
             height="250"
             border-color="#eaeaea"
