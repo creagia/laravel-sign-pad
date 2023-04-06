cc4e98a93b8558a8a90475cbbc2f1b62b0f0e31a
Marc <marc@creagia.com>
2023-04-06 18:50:58 +0200

    Allow multiple Sign Pads

--- resources/assets/sign-pad.js
+++ resources/assets/sign-pad.js
@@ -1,31 +1,36 @@
 import SignaturePad from 'signature_pad';
 
-const eSignpad = document.getElementById('e-signpad');
+const eSignpads = document.querySelectorAll('.e-signpad');
 
-let signaturePad = new SignaturePad(eSignpad.querySelector('canvas')),
-    submit = document.getElementById('sign-pad-button-submit'),
-    clear = document.getElementById('sign-pad-button-clear');
+eSignpads.forEach(function(eSignpad) {
+    let signaturePad = new SignaturePad(eSignpad.querySelector('canvas')),
+        submit = eSignpad.querySelector('.sign-pad-button-submit'),
+        clear = eSignpad.querySelector('.sign-pad-button-clear');
 
-submit.addEventListener('click', function () {
-    document.getElementById('sign').value = signaturePad.toDataURL();
+    submit.addEventListener('click', function () {
+        eSignpad.querySelector('.sign').value = signaturePad.toDataURL();
+    });
+
+    clear.addEventListener('click', function () {
+        signaturePad.clear();
+    })
+
 });
 
-clear.addEventListener('click', function () {
-    signaturePad.clear();
-})
-
 let resizeCanvas = () => {
+    document.querySelectorAll('.e-signpad').forEach(function(eSignPad) {
+        let canvas = eSignPad.querySelector('canvas');
 
-    let canvas = document.getElementById('e-signpad').querySelector('canvas');
+        if (canvas.width > window.innerWidth) {
+            let signaturePad = new SignaturePad(eSignpad.querySelector('canvas'));
+            const ratio = Math.max(window.devicePixelRatio || 1, 1);
 
-    if (canvas.width > window.innerWidth) {
-        const ratio = Math.max(window.devicePixelRatio || 1, 1);
-
-        canvas.width = canvas.offsetWidth * ratio;
-        canvas.height = canvas.offsetHeight * ratio;
-        canvas.getContext("2d").scale(ratio, ratio);
-        signaturePad.clear();
-    }
+            canvas.width = canvas.offsetWidth * ratio;
+            canvas.height = canvas.offsetHeight * ratio;
+            canvas.getContext("2d").scale(ratio, ratio);
+            signaturePad.clear();
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function (event) {
